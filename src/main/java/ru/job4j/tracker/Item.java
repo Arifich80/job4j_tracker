0f26366673c95e05d63cadaec1dbb290ab3055d7
Arifich80 <arifich@yahoo.com>
2022-12-20 17:11:32 +0300

    Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,15 @@
 package ru.job4j.tracker;
+import java.time.LocalDateTime;
 
 public class Item {
     private int id;
     private String name;
+
+    LocalDateTime created = LocalDateTime.now();
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 
     public Item() {
 
@@ -35,3 +42,4 @@ public class Item {
         this.name = name;
     }
 }
+
