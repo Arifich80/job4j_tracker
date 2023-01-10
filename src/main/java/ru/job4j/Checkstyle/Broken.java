76802ec6242a96003279833fe4033c971897c14f
Arifich80 <arifich@yahoo.com>
2023-01-10 17:53:39 +0300

    Правила Checkstyle [#504981 #379195]

--- src/main/java/ru/job4j/Checkstyle/Broken.java
+++ src/main/java/ru/job4j/Checkstyle/Broken.java
@@ -1,3 +1,4 @@
+
 package ru.job4j.Checkstyle;
 
 public class Broken {
@@ -8,15 +9,17 @@ public class Broken {
 
     public Broken() {
     }
-            public void method(int a, int b, int c, int d, int e, int f, int g) {
-                Object obj = new Object();
-                   void media(Object obj) {
-                    if (obj != null) {
-                        System.out.println(obj);
-                        void echo(); {
-                        }
 
-            }
+    void echo() {
+    }
+    void Media (Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
         }
+
+         void method(int a, int b, int c, int d, int e, int f, int g) {
+        }
+
+
     }
 }
