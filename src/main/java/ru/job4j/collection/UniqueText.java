f441b50288d339f68b84b4a7cfaec8b4be6d53d3
Arifich80 <arifich@yahoo.com>
2023-02-01 00:02:21 +0300

    Идентичные тексты [#212655 #387014]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String i : text) {
             if (!check.contains(i)) {
                 rsl = false;
+                break;
             }
         }
         return rsl;
