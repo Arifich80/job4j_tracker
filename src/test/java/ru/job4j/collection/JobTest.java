1cdf5f2ec0e4584c940121e9059806c6e3a2c6fd
Arifich80 <arifich@yahoo.com>
2023-02-10 13:17:10 +0300

    Комбинированный компаратор [#10036 #390157]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -1,7 +1,12 @@
 package ru.job4j.collection;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JobTest {
@@ -33,5 +38,69 @@ public class JobTest {
                 new Job("Fix bug", 2)
         );
         assertThat(rsl).isEqualTo(0);
+    }
+
+    @Test
+    public void whenJobAscByName() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2),
+                new Job("Fix bugs", 4)
+        );
+        Collections.sort(jobs, new JobAscByName());
+        List<Job> expected = Arrays.asList(
+                new Job("Fix bugs", 4),
+                new Job("Impl task", 2),
+                new Job("Reboot server", 1)
+        );
+        assertThat(jobs).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenJobDescByName() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2),
+                new Job("Fix bugs", 4)
+        );
+        Collections.sort(jobs, new JobDescByName());
+        List<Job> expected = Arrays.asList(
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2),
+                new Job("Fix bugs", 4)
+        );
+        assertThat(jobs).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenJobAscByPriority() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2),
+                new Job("Fix bugs", 4)
+        );
+        Collections.sort(jobs, new JobAscByPriority());
+        List<Job> expected = Arrays.asList(
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2),
+                new Job("Fix bugs", 4)
+        );
+        assertThat(jobs).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenJobDescByPriority() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2),
+                new Job("Fix bugs", 4)
+        );
+        Collections.sort(jobs, new JobDescByPriority());
+        List<Job> expected = Arrays.asList(
+                new Job("Fix bugs", 4),
+                new Job("Impl task", 2),
+                new Job("Reboot server", 1)
+        );
+        assertThat(jobs).isEqualTo(expected);
     }
 }
