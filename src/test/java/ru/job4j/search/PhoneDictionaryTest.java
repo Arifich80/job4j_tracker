77ab80457b68a3a848dec98b3a5dcde92ebd8c14
Arifich80 <arifich@yahoo.com>
2023-03-02 16:51:22 +0300

    Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -3,7 +3,6 @@ package ru.job4j.search;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,18 +17,3 @@ public class PhoneDictionaryTest {
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 }
-/**
-    @Test
-    public void whenNotFound() {
-        String rsl = "not found";
-        PhoneDictionary phones = new PhoneDictionary();
-        phones.add(
-                new Person("Alex", "Ivanov", "123456", "Moscow")
-        );
-        List<Person> persons = findPerson;
-        List<Person> emptyList = new ArrayList();
-        ArrayList<Person> persons = phones.find("Petr");
-        assertThat(persons, is(emptyList));
-    }
-}
- **/
