c3698fa56c149ab02be2eb186d41e527b5b6217c
Arifich80 <arifich@yahoo.com>
2023-03-09 21:29:52 +0300

    Аттестация [#504878]

---
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -0,0 +1,74 @@
+package ru.job4j.hmap;
+
+import java.util.*;
+
+public class AnalyzeByMap {
+
+    public static double averageScore(List<Pupil> pupils) {
+        double sum = 0;
+        int counter = 0;
+        for (Pupil pupil : pupils) {
+            for (Subject subj : pupil.subjects()) {
+                sum += subj.score();
+                counter++;
+            }
+        }
+        return sum / counter;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        List<Label> list = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double sum = 0;
+            for (Subject subj : pupil.subjects()) {
+                sum += subj.score();
+            }
+            list.add(new Label(pupil.name(), sum / pupil.subjects().size()));
+        }
+        return list;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        Map<String, Double> temp = new LinkedHashMap<>();
+        List<Label> list = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subj : pupil.subjects()) {
+                temp.computeIfPresent(subj.name(), (k, v) -> v + subj.score());
+                temp.putIfAbsent(subj.name(), (double) subj.score());
+            }
+        }
+        for (Map.Entry<String, Double> entry : temp.entrySet()) {
+            list.add(new Label(entry.getKey(), entry.getValue() / pupils.size()));
+        }
+        return list;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        List<Label> list = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            double sum = 0;
+            for (Subject subj : pupil.subjects()) {
+                sum += subj.score();
+            }
+            list.add(new Label(pupil.name(), sum));
+        }
+        list.sort(Comparator.comparing(Label::score));
+        return list.get(list.size() - 1);
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        Map<String, Double> temp = new LinkedHashMap<>();
+        List<Label> list = new ArrayList<>();
+        for (Pupil pupil : pupils) {
+            for (Subject subj : pupil.subjects()) {
+                temp.computeIfPresent(subj.name(), (k, v) -> v + subj.score());
+                temp.putIfAbsent(subj.name(), (double) subj.score());
+            }
+        }
+        for (Map.Entry<String, Double> entry : temp.entrySet()) {
+            list.add(new Label(entry.getKey(), entry.getValue()));
+        }
+        list.sort(Comparator.comparing(Label::score));
+        return list.get(list.size() - 1);
+    }
+}
